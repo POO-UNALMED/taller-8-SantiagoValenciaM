3f3be507e024e8f4203c814733e55a35a0383b46
Santiago Valencia Mejía <savalencia@unal.edu.co>
2020-10-17 09:08:22 -0500

    Final V2.0

---
+++ src/main/java/futbol/Futbolista.java
@@ -0,0 +1,52 @@
+package futbol;
+import java.lang.Comparable;
+
+public class Futbolista implements Comparable<Object>{
+	private String nombre;
+	private int edad;
+	private final String posicion;
+	
+	
+	public Futbolista() {
+		this("Maradona", 30, "delantero");
+	}
+	
+	public Futbolista(String nombre, int edad, String posicion) {
+		this.nombre = nombre; this.edad = edad; this.posicion = posicion;
+	}
+	
+	public String toString() {
+		return "El futbolista "+ nombre +" tiene " + edad + ", y juega de " + posicion;
+	}
+	
+	public int compareTo(Object futbolista) {
+		if(this.equals(futbolista)) {
+			return 1;
+		}else {
+			    return 0;
+			    }
+		}
+	
+	public String getPosicion() {
+        return posicion;
+    }
+    public int getEdad() {
+        return this.edad;
+    }
+    
+	public boolean jugarConLasManos() {
+		return false;
+	}
+	
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    public String getNombre() {
+        return this.nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,34 @@
+package futbol;
+
+public class Portero extends Futbolista{
+	public short golesRecibidos;
+	public byte dorsal;
+	
+	public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
+		super(nombre, edad, "Portero");
+		this.golesRecibidos = golesRecibidos; this.dorsal = dorsal;
+	}
+	
+	public String toString() {
+		return "El futbolista " + this.getNombre() + " tiene " + this.getEdad() + ", y juega de " + this.getPosicion() +
+		    	" con el dorsal " + this.getDorsal() + ". Le han marcado " + this.getGolesRecibidos();
+	}
+	
+	public int compareTo(Portero portero) {
+		return (portero.getGolesRecibidos() -this.getGolesRecibidos());
+	}
+
+	public short getGolesRecibidos() {
+		return this.golesRecibidos;
+	}
+	
+	public boolean jugarConLasManos() {
+		return true;
+	}
+	
+	public byte getDorsal() {
+		return this.dorsal;
+	}
+	
+	
+}
